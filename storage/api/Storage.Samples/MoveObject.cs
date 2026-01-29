f856bd8b0d88eaa802d0167a794950e1c7cebd3f
Nidhi <nidhiii@google.com>
2026-01-29 12:46:12 +0000

    samples: feat(storage):- MOVING OBJECTS FOR ALL BUCKET TYPES

--- storage/api/Storage.Samples/MoveObject.cs
+++ storage/api/Storage.Samples/MoveObject.cs
@@ -1,38 +1,33 @@
-// Copyright 2025 Google LLC
+// Copyright 2022 Google LLC
 //
-// Licensed under the Apache License, Version 2.0 (the "License").
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at 
+// You may obtain a copy of the License at
 //
-// https://www.apache.org/licenses/LICENSE-2.0 
+//     https://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software 
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and 
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 // [START storage_move_object]
-
 using Google.Cloud.Storage.V1;
 using System;
 
 public class MoveObjectSample
 {
-    /// <summary>
-    /// Moves an object within a bucket with hierarchical namespace enabled.
-    /// </summary>
-    /// <param name="sourceBucketName">Name of the source bucket containing the object to move.</param>
-    /// <param name="sourceObjectName">The name of the source object to move within the bucket.</param>
-    /// <param name="destinationObjectName">The name of the new object to move to within the bucket.</param>
     public void MoveObject(
-        string sourceBucketName = "source-bucket-name",
-        string sourceObjectName = "source-object-name",
-        string destinationObjectName = "destination-object-name")
+        string sourceBucketName = "your-source-bucket",
+        string sourceObjectName = "your-source-object",
+        string destinationBucketName = "your-destination-bucket",
+        string destinationObjectName = "your-destination-object")
     {
         var storage = StorageClient.Create();
-        storage.MoveObject(sourceBucketName, sourceObjectName, destinationObjectName);
-        Console.WriteLine($"Moved {sourceBucketName}/{sourceObjectName} to " + $"{sourceBucketName}/{destinationObjectName} within a hierarchical namespace enabled bucket.");
+        storage.MoveObject(sourceBucketName, sourceObjectName, destinationBucketName, destinationObjectName);
+
+        Console.WriteLine($"Moved {sourceObjectName} from {sourceBucketName} to {destinationObjectName} in {destinationBucketName}.");
     }
 }
 // [END storage_move_object]
