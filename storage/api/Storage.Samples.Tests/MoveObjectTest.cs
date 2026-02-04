2e407ffc53c27d8a75bde859157068634c247040
Nidhi <nidhiii@google.com>
2026-02-04 05:08:04 +0000

    chore: add tests

--- storage/api/Storage.Samples.Tests/MoveObjectTest.cs
+++ storage/api/Storage.Samples.Tests/MoveObjectTest.cs
@@ -2,14 +2,14 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License").
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at 
+// You may obtain a copy of the License at
 //
-// https://www.apache.org/licenses/LICENSE-2.0 
+// https://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software 
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and 
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 using System;
@@ -34,10 +34,10 @@ public class MoveObjectTest
         var originName = Guid.NewGuid().ToString();
         var originContent = Guid.NewGuid().ToString();
         var destinationName = Guid.NewGuid().ToString();
-        uploadObjectFromMemory.UploadObjectFromMemory(_fixture.BucketNameHns, originName, originContent);
-        moveObjectSample.MoveObject(_fixture.BucketNameHns, originName, destinationName);
-        _fixture.CollectHnsObject(destinationName);
-        var objects = listFilesSample.ListFiles(_fixture.BucketNameHns);
+        uploadObjectFromMemory.UploadObjectFromMemory(_fixture.BucketNameGeneric, originName, originContent);
+        moveObjectSample.MoveObject(_fixture.BucketNameGeneric, originName, destinationName);
+        _fixture.Collect(destinationName);
+        var objects = listFilesSample.ListFiles(_fixture.BucketNameGeneric);
         Assert.DoesNotContain(objects, obj => obj.Name == originName);
         Assert.Contains(objects, obj => obj.Name == destinationName);
     }
